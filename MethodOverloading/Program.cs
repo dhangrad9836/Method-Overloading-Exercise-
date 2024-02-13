acad6dac10bf71a874f79216268cc49196f3821f
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-02-13 11:48:05 -0500

    Completed method overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 0, true));
+            Console.WriteLine(Add(1,0, false));
+            Console.WriteLine(Add(1.0M, 5.2M));
+
+            Console.ReadLine();
+
+        } //end of main method
+
+        //overloaded add methods
+        public static int Add(int num1, int num2)
+        {
+            //return the sum of both integers
+            return num1 + num2;
         }
-    }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            //return the sum of both decimal
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            //return an int value if bool type is true
+            if (isTrue == true)
+            {
+                //append word: dollar if total is equal to 1
+                if (num2 + num1 == 1)
+                {
+                    return "1 dollar";
+                }
+                //append word: dollars if total is greater than 1
+                else if (num1 + num2 > 1)
+                {
+                    return num1 + num2 + " dollars";
+                }
+            }
+            //if false return "false"
+            return "false";
+            
+            
+        }
+    }// end of Class
 }
